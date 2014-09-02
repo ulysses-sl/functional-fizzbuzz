32eed15cc20a3745847129f3e17afc812890af1a
ulysses-sl <mail@saklee.net>
2014-09-01 22:18:06 -0400

    Haskell version

--- cracklepop.hs
+++ cracklepop.hs
@@ -5,9 +5,8 @@ cpop :: Int -> Bool -> [(Int, [Char])] -> [Char]
 cpop num divisible [] = if divisible then [] else show num
 
 cpop num old_div ((x, y):xs) = word ++ cpop num (old_div || new_div) xs
-    where
-        new_div = rem num x == 0
-        word = if new_div then y else []
+    where new_div = rem num x == 0
+          word = if new_div then y else []
 
 main :: IO [()]
 main = mapM (putStrLn . cracklepop) [1..100]
