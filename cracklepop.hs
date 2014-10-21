6a72fd9ac3eaded2402d8ca8d17f16adcb0360d6
ulysses-sl <mail@saklee.net>
2014-10-20 21:05:29 -0400

    Haskell ver cleared up

--- cracklepop.hs
+++ cracklepop.hs
@@ -1,22 +1,13 @@
 cplist = [(3, "Crackle"), (5, "Pop")]
 
-cracklepop :: Int -> [Char]
-cracklepop num = take_nonempty ((flatten . cpop) num) (show num)
+cracklepop num = putStrLn (take_nonempty (cpop num) (show num))
 
-cpop :: Int -> [[Char]]
-cpop num = map (div_check num) cplist
-    where div_check n (d, word) = if n `divisible_by` d then word else []
+cpop num = cplist >>= (cpop' num)
+cpop' num (d, word) = if num `divisible_by` d then word else ""
 
-take_nonempty :: [a] -> [a] -> [a]
 take_nonempty [] ys = ys
 take_nonempty xs ys = xs
 
-divisible_by :: Int -> Int -> Bool
 divisible_by num x = rem num x == 0
 
-flatten :: [[a]] -> [a]
-flatten [] = []
-flatten xs = foldl (++) [] xs
-
-main :: IO [()]
-main = mapM (putStrLn . cracklepop) [1..100]
+main = mapM cracklepop [1..100]
